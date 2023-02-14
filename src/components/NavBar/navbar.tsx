5ba1d73d56e34f4dccfe329fe9465a9c9f502792
Catalina Sabogal <c.sabogalortiz@gmail.com>
2023-02-14 12:32:15 +0100

    Navbar enabled routes

--- src/components/NavBar/navbar.tsx
+++ src/components/NavBar/navbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom"
 import { useState } from "react";
 import {
   BsArrowLeftShort,
@@ -19,6 +20,7 @@ interface Menu {
   spacing?: boolean;
   submenu?: boolean;
   submenuItems?: SubmenuItem[];
+  Linkto?: string
 }
 
 interface SubmenuItem {
@@ -31,10 +33,39 @@ const Navbar: React.FC = () => {
   const [submenuOpen, setsubmenuOpen] = useState<boolean>(false);
 
   const Menus: Menu[] = [
-    { title: "MarketPlace", spacing: true, icon: <AiOutlineShop /> },
-    { title: "Profile", icon: <BsFillPersonFill /> },
-    { title: "Orders", icon: <BsCart4 /> },
-    { title: "Messages", icon: <BsEnvelope /> },
+
+    {
+      title: "MarketPlace", spacing: true,
+      icon: (
+        <Link to="/marketplace">
+          <AiOutlineShop />
+        </Link>
+      )
+    },
+    {
+      title: "Profile", icon: (
+        <Link to="/profile/:user_id">
+          <BsFillPersonFill />
+        </Link>
+      )
+    },
+    {
+      title: "Shopping Cart", icon: (
+        <Link to="/cart">
+          <BsCart4 />
+        </Link>
+      ),
+    },
+    {
+      title: "Messages", icon: (
+        <Link to="/messages">
+          <BsEnvelope />
+        </Link>
+      ),
+    },
+
+
+
     {
       title: "My Shops",
       submenu: true,
@@ -44,52 +75,51 @@ const Navbar: React.FC = () => {
         { title: "Shop3" },
       ],
     },
-    { title: "Settings", spacing: true, icon: <CiSettings /> },
+    {
+      title: "Settings", spacing: true, icon: (
+        <Link to="/settings">
+          <CiSettings />
+        </Link>
+      )
+    },
   ];
   return (
     <div className="flex">
       <div
-        className={`bg-dark-blue h-screen p-5 pt-8 ${
-          open ? "w-72" : "w-20"
-        } duration-300 relative`}
+        className={`bg-dark-blue h-screen p-5 pt-8 ${open ? "w-72" : "w-20"
+          } duration-300 relative`}
       >
         <BsArrowLeftShort
-          className={`bg-white text-dark-blue text-3xl rounded-full absolute -right-3 top-9 border-dark-blue cursor-pointer ${
-            !open && "rotate-180"
-          }
+          className={`bg-white text-dark-blue text-3xl rounded-full absolute -right-3 top-9 border-dark-blue cursor-pointer ${!open && "rotate-180"
+            }
           `}
           onClick={() => setOpen(!open)}
         />
         <div className="inline-flex">
           <FaUserAstronaut
-            className={`bg-white text-4xl rounded cursor-pointer block float-left mr-2 duration-500 ${
-              open && "rotate-[360deg]"
-            }`}
+            className={`bg-white text-4xl rounded cursor-pointer block float-left mr-2 duration-500 ${open && "rotate-[360deg]"
+              }`}
           />
           <h1
-            className={`text-white origin-left font-medium text-2xl duration-300 ${
-              !open && "scale-0"
-            }`}
+            className={`text-white origin-left font-medium text-2xl duration-300 ${!open && "scale-0"
+              }`}
           >
             Appura
           </h1>
         </div>
         <div
-          className={`flex items-center rounded-md bg-light-blue mt-6  ${
-            !open ? "px-2.5" : "py-4"
-          } py-2`}
+          className={`flex items-center rounded-md bg-light-blue mt-6  ${!open ? "px-2.5" : "py-4"
+            } py-2`}
         >
           <BsSearch
-            className={`text-white text-lg block float-left cursor-pointer ${
-              open && "mr-2 ml-4"
-            }`}
+            className={`text-white text-lg block float-left cursor-pointer ${open && "mr-2 ml-4"
+              }`}
           />
           <input
             type={"search"}
             placeholder="Search"
-            className={`text-base bg-transparent w-full text-white focus:outline-none ${
-              !open && "hidden"
-            }`}
+            className={`text-base bg-transparent w-full text-white focus:outline-none ${!open && "hidden"
+              }`}
           />
         </div>
         <ul className="pt-7">
@@ -97,17 +127,15 @@ const Navbar: React.FC = () => {
             <>
               <li
                 key={index}
-                className={`text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-blue rounded-md mt-2 ${
-                  menu.spacing ? "mt-9" : "mt-2"
-                }`}
+                className={`text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-blue rounded-md mt-2 ${menu.spacing ? "mt-9" : "mt-2"
+                  }`}
               >
                 <span className="text-2xl block float-left">
                   {menu.icon ? menu.icon : <MdDashboard />}
                 </span>
                 <span
-                  className={`text-base font-medium flex-1 duration-200 ${
-                    !open && "hidden"
-                  }`}
+                  className={`text-base font-medium flex-1 duration-200 ${!open && "hidden"
+                    }`}
                 >
                   {menu.title}
                 </span>
